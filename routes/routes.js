cb43fdbbc4ea6ab7a168573079ef24762dfc3241
Anyi Lorena Gonzalez <lorena0118a@gmail.com>
2020-12-17 23:05:26 -0500

    Changes realized, new example

--- routes/routes.js
+++ routes/routes.js
@@ -2,73 +2,70 @@ const {Router} = require('express');
 const router = Router();
 const fs = require('fs');
 
-const moviesFile = fs.readFileSync("./movies.json","utf8");
-let movies = JSON.parse(moviesFile);
+const carsFile = fs.readFileSync("./cars.json","utf8");
+let cars = JSON.parse(carsFile);
 
 router.get("/",(req,res)=>{
     res.json("Bienvenido a mi API");
 });
 
-router.get("/movies",(req,res)=>{
-  res.status(200).json(movies);
+router.get("/cars",(req,res)=>{
+  res.status(200).json(cars);
 });
 
-router.post("/movies",(req,res)=>{
+router.post("/cars",(req,res)=>{
     
-  const { title, director, year, duration, genre, poster} = req.body;
+  const {PLACA,MARCA,MODELO,USUARIO} = req.body
 
-  if(!title || !director || !year || !duration || !genre || !poster ){
+  if(!PLACA || !MARCA || !MODELO || !USUARIO){
     res.status(401).json({error:"Por favor, diligencie todos los datos"});
   }else{
 
-  const id = movies.length + 1;
+  const id = cars.length + 1;
 
 
-  let  newMovie = {
+  let  newCar = {
     id,
-    title,
-    director,
-    year,
-    duration,
-    genre,
-    poster
+    PLACA,
+    MARCA,
+    MODELO,
+    USUARIO
   };
 
-  movies.push(newMovie);
-  const json_movies = JSON.stringify(movies);
+  cars.push(newCar);
+  const json_cars = JSON.stringify(cars);
 
-  fs.writeFileSync("./movies.json", json_movies, "utf-8");
+  fs.writeFileSync("./cars.json", json_cars, "utf-8");
 
-   res.status(200).json(movies);
+   res.status(200).json(cars);
 
   }
 });
 
-router.put("/movies/:id",(req,res)=>{
+router.put("/cars/:id",(req,res)=>{
 
-  const { title, director, year, duration, genre, poster}=  req.body;
+  const {PLACA,MARCA,MODELO,USUARIO} = req.body
   const id = req.params.id;
    
-  if(!title || !director || !year || !duration || !genre || !poster || !id){
+  if(!PLACA || !MARCA || !MODELO || !USUARIO || !id ){
     res.status(401).json({error:"Debe completar los datos y especificar el id."});
   }else{
      
-    movies.filter((movie)=>{
+    cars.filter((car)=>{
 
-     if(movie.id == id){
-       movie.title = title;
-       movie.director = director;
-       movie.year = year;
-       movie.duration = duration;
-       movie.genre = genre;
-       movie.poster = poster;
+     if(car.id == id){
+      car.PLACA = PLACA;
+      car.MARCA = MARCA;
+      car.MODELO = MODELO;
+      car.USUARIO = USUARIO;
+       
      }
     }); 
 
-    const json_movies = JSON.stringify(movies);
-    fs.writeFileSync("./movies.json",json_movies,"utf-8");
+    const json_cars = JSON.stringify(cars);
+    fs.writeFileSync("./cars.json",json_movies,"utf-8");
 
-    res.status(200).json(movies);
+    res.status(200).json(cars);
 
 
   }
@@ -78,19 +75,19 @@ router.put("/movies/:id",(req,res)=>{
 });
 
 
-router.delete("/movies/:id",(req,res)=>{
+router.delete("/cars/:id",(req,res)=>{
     const id = req.params.id;
 
     if(!id){
       res.status(401).json({error: "Especifique un id"});
     }else{
-      const indexMovie = movies.findIndex((movie) => movie.id === id);
-      movies.splice(indexMovie,1);
+      const indexCar = cars.findIndex((car) => car.id === id);
+      cars.splice(indexCar,1);
 
-      const json_movies = JSON.stringify(movies);
-      fs.writeFileSync("./movies.json", json_movies,"utf-8");
+      const json_cars = JSON.stringify(cars);
+      fs.writeFileSync("./cars.json", json_cars,"utf-8");
 
-      res.status(200).json(movies);
+      res.status(200).json(cars);
 
      
     }
